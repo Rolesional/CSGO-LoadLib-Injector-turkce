04082222688b58ce8da02356117501eba31eb499
Vizyon <Vizyon@MASA-44>
2022-09-24 15:29:26 +0300

    Proje dosyası ekle.

---
+++ Rolesyazilim loader/Loader.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace Rolesyazilim_loader
+{
+    partial class Loader
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Loada = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.BackColor = System.Drawing.Color.White;
+            this.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Button1.Location = new System.Drawing.Point(12, 69);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(253, 34);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "Inject Et";
+            this.Button1.UseVisualStyleBackColor = false;
+            this.Button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Loada
+            // 
+            this.Loada.AutoSize = true;
+            this.Loada.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Loada.ForeColor = System.Drawing.Color.White;
+            this.Loada.Location = new System.Drawing.Point(8, 9);
+            this.Loada.Name = "Loada";
+            this.Loada.Size = new System.Drawing.Size(200, 22);
+            this.Loada.TabIndex = 1;
+            this.Loada.Text = "Rolesional Injector";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 14);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Made By Rolesional";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(245, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(23, 21);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Loader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(275, 113);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Loada);
+            this.Controls.Add(this.Button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Loader";
+            this.Text = "RolesYazılım Loader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Label Loada;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Rolesyazilim loader/Loader.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+namespace Rolesyazilim_loader
+{
+    public partial class Loader : Form
+    {
+        string dll_ismi = "win68dll";
+        int beklemesüresi = 40000;
+
+        public Loader()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Button1.Text = "Başlatılıyor";
+
+            WebClient wb = new WebClient();
+            string mainpath = "C:\\Windows\\" + dll_ismi + ".dll";
+            wb.DownloadFile("İndirme Bağlantısını Buraya Yerleştiriniz.", mainpath);
+
+            Button1.Text = "Oyun Açılıyor...";
+
+            Process.Start("steam://rungameid/730");
+            await Task.Delay(beklemesüresi);
+            Process csgo = Process.GetProcessesByName("csgo").FirstOrDefault();
+            Process[] csgo_array = Process.GetProcessesByName("csgo");
+            if (csgo_array.Length != 0)
+            {
+                Button1.Text = "Inject Ediliyor...";
+                this.Hide();
+                MessageBox.Show("Inject Başarılı!", "Rolesional Injector");
+
+                Inject.InjectMethod.Inject(mainpath, "csgo");
+                Console.Read();
+                csgo.WaitForExit();
+
+                if (File.Exists(mainpath))
+                {
+                    File.Delete(mainpath);
+                }
+                Environment.Exit(0);
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
+
+
+
+
+
+
